8e75be33be43be3467c95c13c2aef9706de6f0fd
Klay <arqkdias@gmail.com>
2024-01-03 16:17:05 -0400

    doc: ATT

---
+++ desafio.js
@@ -0,0 +1,16 @@
+let distancia =  100;
+let gasto_medio = 10;
+const CombustivelGasolina = 5.08;
+const CombustivelAlcool = 3.08;
+let TipoCombustivel = 'Etanol';
+
+
+const distanciaPercorrida = distancia / gasto_medio
+if (TipoCombustivel === 'Gasolina') {
+    let ValorGasto = distanciaPercorrida * CombustivelGasolina;
+    console.log(ValorGasto.toFixed(2))
+} else {
+    let ValorGasto = distanciaPercorrida * CombustivelAlcool;
+    console.log(ValorGasto.toFixed(2))
+}
+
