636fcb67d8e4ff97344a2e890c1e109c317cac14
Klay <arqkdias@gmail.com>
2024-01-03 16:29:48 -0400

    doc: att

---
+++ exercicio1.js
@@ -0,0 +1,14 @@
+
+const nota1 = 7;
+const nota2 = 6;
+const nota3 = 4;
+
+const media = (nota1 + nota2 + nota3) / 3;
+
+if (media < 5) {
+    console.log('Reprovado')
+} else if (media >= 5 or media <= 7) {
+    console.log('Recuperação')
+} else {
+    console.log('Passou de semestre!!')
+}
