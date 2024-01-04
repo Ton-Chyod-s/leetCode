6a8ac3aa3cd817d660f2191797c6687d333c89c6
Klay <arqkdias@gmail.com>
2024-01-04 09:50:31 -0400

    doc: att

---
+++ exercicio2.js
@@ -0,0 +1,17 @@
+
+const PESO = 71;
+const ALTURA = 178;
+
+const IMC = PESO / (ALTURA * ALTURA)
+
+if (IMC < 18.5) {
+    console.log('Abaixo do peso')
+} else if (IMC > 18.5 && IMC < 25) {
+    console.log('Peso normal')
+} else if (IMC > 25 && IMC < 30) {
+    console.log('Acima do peso')
+} else if (IMC > 30 && IMC > 40) {
+    console.log('Obeso')
+} else {
+    console.log('Obesidade Grave!')
+}
