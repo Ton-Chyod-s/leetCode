93789e2c4d81358d385dc13a2d6db9de5a346265
Klay <arqkdias@gmail.com>
2024-01-04 10:26:40 -0400

    doc: att

---
+++ exercicio3.js
@@ -0,0 +1,15 @@
+const PreçoNormal = 75;
+const EscolhaPagamento = 4
+
+if (EscolhaPagamento === 'Débito') {
+    const PrecoDesconto = PreçoNormal - (PreçoNormal * .1)
+    console.log('R$' + PrecoDesconto)
+} else if (EscolhaPagamento === 'Dinheiro' || EscolhaPagamento === 'Pix') {
+    const PrecoDesconto = PreçoNormal - (PreçoNormal * .15)
+    console.log('R$' + PrecoDesconto)
+} else if (EscolhaPagamento === 2 || EscolhaPagamento === 1) {
+    console.log('R$' + PreçoNormal)
+} else {
+    const PrecoDesconto = PreçoNormal + (PreçoNormal * .1)
+    console.log('R$' + PrecoDesconto)
+}
