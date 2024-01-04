cf71f7e56bf4fbe33361e634ec5c23f01d1d258f
Klay <arqkdias@gmail.com>
2024-01-04 13:14:49 -0400

    doc: att

--- aula.js
+++ aula.js
@@ -1,13 +1,1 @@
 
-const numero = 10;
-
-const NumeroPar = (numero % 2) === 0;
-
-
-if (NumeroPar === 0) {
-    console.log('O numero é invalido!!')
-} else if (NumeroPar) {
-    console.log('É par');
-} else {
-    console.log('É impar');
-}
