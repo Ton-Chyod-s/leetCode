322b26bc893a19e152105da907ca7ec255a5bdf5
Klay <arqkdias@gmail.com>
2024-01-09 10:59:31 -0400

    Update 14.Longest-Common-Prefix.js

---
+++ LeetCode/14.Longest-Common-Prefix.js
@@ -0,0 +1,11 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    for (let i in strs) {
+        console.log(strs[i])
+    }
+};
+
+longestCommonPrefix(["flower","flow","flight"])
