a1e004e97b8335252774f9ffaebb00b75b3c9846
Klay <arqkdias@gmail.com>
2024-01-12 09:52:17 -0400

    Update 3.Longest-Substring-Without-Repeating-haracters.js

--- LeetCode/3.Longest-Substring-Without-Repeating-haracters.js
+++ LeetCode/3.Longest-Substring-Without-Repeating-haracters.js
@@ -2,6 +2,21 @@
  * @param {string} s
  * @return {number}
  */
+let caracterRepetido = []
+
 var lengthOfLongestSubstring = function(s) {
+    const valorUnico = [...new Set(s)]
+
+    for (let i = 0; i < unique.length; i++) {
+        console.log(valorUnico[i])
+        
+    }
     
-};
+      
+        
+    } 
+    
+   
+
+
+console.log(lengthOfLongestSubstring("pwwkew"))
