8bdd66bf61ca2fc910f8780c56f61658c9de9fb0
Klay <arqkdias@gmail.com>
2024-01-15 15:32:31 -0400

    Update exer Exibir uma coleção de discos.js

--- exer Exibir uma coleção de discos.js
+++ exer Exibir uma coleção de discos.js
@@ -20,20 +20,44 @@ const recordCollection = {
   };
   
   // Altere apenas o código abaixo desta linha
-  function updateRecords(records, id, prop, value) {
-    artista = recordCollection[id][prop]
-    if (isNaN(artista)) {
-        recordCollection[id][prop] = value
+   /* function updateRecords(records, id, prop, value) {
+    if (isNaN(records[id][prop])) {
+        records[id][prop] = value
     } else if (prop === 'tracks') {
         if (isNaN(value)) {
-            recordCollection[id][prop] = Array(value)
+            records[id][prop] = Array(value)
         } else {
-            recordCollection[id][prop] = ""        
+            records[id][prop] = ""        
         }
         
     }
     return records;
-  }
+  } */
   
-  console.log(updateRecords(recordCollection, 5439, 'artist', 'ABBA'));
-  console.log(updateRecords(recordCollection, 5439, "tracks", ""))
+    // Altere apenas o código abaixo desta linha
+    function updateRecords(records, id, prop, value) {
+        tamanho = Object.keys(records[id]).length
+        tamanhoTracks = records[id][prop]
+        if (tamanho === 1) {
+            records[id][prop] = value
+        } else {
+            if (isNaN(records[id][prop])) {
+                if (records.hasOwnProperty(prop)) {
+                    records[id][prop].push(value)
+                } else {
+                    records[id][prop] = value
+                }
+                
+            } else {
+                delete records[id][prop]
+            }
+        }
+        return records;
+    }
+
+
+
+  console.log(updateRecords(recordCollection, 5439, "artist", "ABBA"));
+  console.log(recordCollection[5439]['artist'])
+  console.log(updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me"))
+  console.log(updateRecords(recordCollection, 2548, "artist", ""))
