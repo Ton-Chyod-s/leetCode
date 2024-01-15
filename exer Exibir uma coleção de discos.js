52fd66f2aaf4d120aa8f18b9c689e9dfbd6374f3
Klay <arqkdias@gmail.com>
2024-01-15 15:52:01 -0400

    Update exer Exibir uma coleção de discos.js

--- exer Exibir uma coleção de discos.js
+++ exer Exibir uma coleção de discos.js
@@ -18,22 +18,7 @@ const recordCollection = {
       albumTitle: 'ABBA Gold'
     }
   };
-  
-  // Altere apenas o código abaixo desta linha
-   /* function updateRecords(records, id, prop, value) {
-    if (isNaN(records[id][prop])) {
-        records[id][prop] = value
-    } else if (prop === 'tracks') {
-        if (isNaN(value)) {
-            records[id][prop] = Array(value)
-        } else {
-            records[id][prop] = ""        
-        }
-        
-    }
-    return records;
-  } */
-  
+
     // Altere apenas o código abaixo desta linha
     function updateRecords(records, id, prop, value) {
         tamanho = Object.keys(records[id]).length
@@ -45,19 +30,28 @@ const recordCollection = {
                 if (records.hasOwnProperty(prop)) {
                     records[id][prop].push(value)
                 } else {
-                    records[id][prop] = value
+                    if (prop === 'artist' && value !== '') {
+                        records[id][prop] = value
+                    } else if (prop === 'tracks' && value !== '') {
+                        records[id][prop] = value
+                    }
                 }
-                
             } else {
-                delete records[id][prop]
+                if (records.hasOwnProperty(prop) === false) {
+                    records[id][prop] = value
+                } else {
+                    delete records[id][prop]
+                }
             }
         }
         return records;
     }
 
-
-
   console.log(updateRecords(recordCollection, 5439, "artist", "ABBA"));
   console.log(recordCollection[5439]['artist'])
   console.log(updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me"))
+  console.log(recordCollection[5439]['tracks'])
   console.log(updateRecords(recordCollection, 2548, "artist", ""))
+  console.log(recordCollection[2548]['artist'])
+  console.log(updateRecords(recordCollection, 1245, "tracks", "Addicted to Love"))
+  console.log(recordCollection[1245]['tracks'])
