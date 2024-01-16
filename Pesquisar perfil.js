86ce2b1241a04102c7a8c910457f07790b122b96
Klay <arqkdias@gmail.com>
2024-01-16 14:14:51 -0400

    Update Pesquisar perfil.js

--- Pesquisar perfil.js
+++ Pesquisar perfil.js
@@ -28,37 +28,34 @@ const contacts = [
   
   function lookUpProfile(name, prop) {
     // Altere apenas o código abaixo desta linha
-    for (let j = 0; j < contacts.length; j++) {
-        for (let i = 0; i < contacts[j].length; i++) {
-            const nome = contacts[i].firstName
-            const ultimoNome = contacts[i].lastName
-            const numero = contacts[i].number
-            const gostar = contacts[i].likes
-                
-            if (name === contacts[i].firstName && contacts[i].hasOwnProperty(prop)) {
-                switch (prop) {
-                    case 'firstName':
-                        return contacts[i].firstName;
-                    case 'lastName':
-                        return contacts[i].lastName;
-                    case 'number':
-                        return contacts[i].number;
-                    case 'likes':
-                        return contacts[i].likes;
-                } break
-            } else if (name !== contacts[i].firstName) {
-                return 'No such contact';
-            } else {
-                return 'No such property';
+    for (let i = 0; i < contacts.length; i++) {
+        if (contacts[i].firstName === name) {
+            for (let j = 0; contacts[i]; j++) { 
+                if (name === contacts[i].firstName && contacts[i].hasOwnProperty(prop)) {
+                    switch (prop) {
+                        case 'firstName':
+                            return contacts[i].firstName;
+                        case 'lastName':
+                            return contacts[i].lastName;
+                        case 'number':
+                            return contacts[i].number;
+                        case 'likes':
+                            return contacts[i].likes;
+                    } break
+                } 
             }
+        } else if (name !== contacts[i].firstName) {
+            return 'No such contact';
+        } else {
+            return 'No such property';
         }
-    }
+}
     // Altere apenas o código acima desta linha
   }
   
-  console.log(lookUpProfile("Kristian", "lastName"));
-  console.log(lookUpProfile("Sherlock", "likes"));
-  console.log(lookUpProfile("Harry", "likes"));
+  //console.log(lookUpProfile("Kristian", "lastName"));
+  //console.log(lookUpProfile("Sherlock", "likes"));
+  //console.log(lookUpProfile("Harry", "likes"));
   console.log(lookUpProfile("Bob", "number"));
   console.log(lookUpProfile("Bob", "potato"));
   console.log(lookUpProfile("Akira", "address"));
