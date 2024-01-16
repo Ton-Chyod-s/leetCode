c724731e2a16b9641eea9c2af3195f1543a80c43
Klay <arqkdias@gmail.com>
2024-01-16 15:44:12 -0400

    Create Usar recursão para criar um intervalo de números melhorada.js

---
+++ Usar recursão para criar um intervalo de números melhorada.js
@@ -0,0 +1,10 @@
+function rangeOfNumbers(startNum, endNum) {
+    if () {
+      return [];
+    } else {
+      return rangeOfNumbers(startNum, endNum)
+    }
+  };
+
+
+  console.log(rangeOfNumbers(1, 5)) //[1, 2, 3, 4, 5]
