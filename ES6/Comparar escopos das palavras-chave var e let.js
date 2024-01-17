ea7f6060b3b58646a5ae1a5b4d1ac2718a2683fb
Klay <arqkdias@gmail.com>
2024-01-17 08:34:41 -0400

    Update Comparar escopos das palavras-chave var e let.js

--- ES6/Comparar escopos das palavras-chave var e let.js
+++ ES6/Comparar escopos das palavras-chave var e let.js
@@ -1,9 +1,11 @@
 function checkScope() {
-    var i = 'function scope';
-    if (true) {
-      i = 'block scope';
-      console.log('Block scope i is: ', i);
-    }
-    console.log('Function scope i is: ', i);
-    return i;
+  let i = 'function scope';
+  if (true) {
+    let i = 'block scope';
+    console.log('Block scope i is: ', i);  
   }
+  console.log('Function scope i is: ', i);
+  return i;
+}
+
+console.log(checkScope(alse))
