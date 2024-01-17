7cc727a1cc87203d4119440767a9c693c28f4e7a
Klay <arqkdias@gmail.com>
2024-01-17 08:47:20 -0400

    Create Modificar um array declarado com const.js

---
+++ ES6/Modificar um array declarado com const.js
@@ -0,0 +1,9 @@
+const s = [5, 7, 2];
+function editInPlace(list) {
+  // Altere apenas o código abaixo desta linha
+  list.revert
+  // Usar s = [2, 5, 7] seria inválido
+
+  // Altere apenas o código acima desta linha
+}
+editInPlace();
