f7936fc9ce5afbf1afda9e89ce8a264b5c96b6db
Klay <arqkdias@gmail.com>
2024-01-17 14:42:00 -0400

    Update Usar getters e setter para controlar acesso a um objeto.js

--- ES6/Usar getters e setter para controlar acesso a um objeto.js
+++ ES6/Usar getters e setter para controlar acesso a um objeto.js
@@ -11,9 +11,9 @@ class Thermostat {
         return this._fahrenheit = (celsius * 9.0) / 5 + 32;
     }
   }
-  // Altere apenas o código acima desta linha
-  
-  const thermos = new Thermostat(76); // Definição na escala Fahrenheit
-  let temp = thermos.temperature; // 24,44 em Celsius
-  thermos.temperature = 26;
-  temp = thermos.temperature; // 26 em Celsius
+// Altere apenas o código acima desta linha
+
+const thermos = new Thermostat(76); // Definição na escala Fahrenheit
+let temp = thermos.temperature; // 24,44 em Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 em Celsius
