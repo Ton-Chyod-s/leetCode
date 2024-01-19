660aa21cef4cefbfe27a90b3ea719a037a8d5cf0
Klay <arqkdias@gmail.com>
2024-01-19 15:45:02 -0400

    Update Iterar através de todos os itens de um array usando laços for.js

--- ES6/Iterar através de todos os itens de um array usando laços for.js
+++ ES6/Iterar através de todos os itens de um array usando laços for.js
@@ -16,9 +16,9 @@ function filteredArray(arr, elem) {
     return newArr;
   }
   
-  // console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3)); // deve retornar []
+  console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3)); // deve retornar []
 
-  console.log(filteredArray([[10, 8, 3], [14, 6, 23], [3, 18, 6]], 18)); // deve retornar [[10, 8, 3], [14, 6, 23]
+  //console.log(filteredArray([[10, 8, 3], [14, 6, 23], [3, 18, 6]], 18)); // deve retornar [[10, 8, 3], [14, 6, 23]
 
   console.log(filteredArray([["trumpets", 2], ["flutes", 4], ["saxophones", 2]], 2)); // deve retornar [["flutes", 4]]
 
