e481de925f73374f26bc55a9857b5c48472b4302
Klay <arqkdias@gmail.com>
2024-01-22 10:25:29 -0400

    Update Encontrar a palavra mais longa em uma string.js

--- Programação de algoritmos básicos/Encontrar a palavra mais longa em uma string.js
+++ Programação de algoritmos básicos/Encontrar a palavra mais longa em uma string.js
@@ -2,11 +2,12 @@ function findLongestWordLength(str) {
   const strLista = str.split(" ")
   count = 0
   for (let i = 0; i < strLista.length; i++) {
-    if (strLista.length > count) {
-      count = strLista.length
+    if (strLista[i].length > count) {
+      count = strLista[i].length
     }
   }
   return count;
 }
 
-findLongestWordLength("The quick brown fox jumped over the lazy dog");
+console.log(findLongestWordLength("Google do a barrel roll"))
+
