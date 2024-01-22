f4ef3061c22c5cca152bb3214c35c301d231c791
Klay <arqkdias@gmail.com>
2024-01-22 11:19:53 -0400

    Update Confirmar o final.js

--- Programação de algoritmos básicos/Confirmar o final.js
+++ Programação de algoritmos básicos/Confirmar o final.js
@@ -1,5 +1,28 @@
 function confirmEnding(str, target) {
-    return str;
+    const verificarEspaçoVazio  = str.indexOf(' ') >= 0;
+    if (verificarEspaçoVazio) { 
+        strLista = str.split(" ")
+    } else {
+        strLista = str.split("")
+    }
+    const ultimoElemento = strLista[strLista.length - 1]
+    if (ultimoElemento.length > 1) {
+        const ultimoElementoLista = ultimoElemento.split("");
+        const ehN = ultimoElementoLista[ultimoElemento.length - 1]
+        if (ehN === 'n') {
+            return true;
+        } else {
+            false;
+        }
+    } else {
+        const ehN = ultimoElementoLista[ultimoElemento.length - 1]
+        if (ehN === 'n') {
+            return true;
+        } else {
+            false;
+        }
+    }
+
   }
   
-  confirmEnding("Bastian", "n");
+  confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification");
