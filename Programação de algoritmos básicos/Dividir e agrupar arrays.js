615e846e60fbbaf5964bc8d7c2ecd2e4eb37fee4
Klay <arqkdias@gmail.com>
2024-01-23 16:04:24 -0400

    Update Dividir e agrupar arrays.js

--- Programação de algoritmos básicos/Dividir e agrupar arrays.js
+++ Programação de algoritmos básicos/Dividir e agrupar arrays.js
@@ -1,5 +1,14 @@
 function chunkArrayInGroups(arr, size) {
-    return arr;
+    const arrayBidimensional = []
+    const novoArray = []
+    for (let i = 0; i < arr.length; i++) {
+        novoArray.push(arr[i])
+        if (novoArray.length === size) {
+            arrayBidimensional.push(novoArray)
+            novoArray.splice(0,2)
+        }
+    }
+    return arrayBidimensional
   }
   
   chunkArrayInGroups(["a", "b", "c", "d"], 2);
