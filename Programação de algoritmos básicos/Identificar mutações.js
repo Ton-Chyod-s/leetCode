3fdd8b36ecde33cd99591be6d95a5e5552d9f647
Klay <arqkdias@gmail.com>
2024-01-23 15:28:07 -0400

    Update Identificar mutações.js

--- Programação de algoritmos básicos/Identificar mutações.js
+++ Programação de algoritmos básicos/Identificar mutações.js
@@ -1,20 +1,13 @@
 function mutation(arr) {
     const primeiraPalavraArr = arr[0].toLowerCase()
     const segundaPlavraArr = arr [1].toLowerCase()
+    const res = primeiraPalavraArr.indexOf(segundaPlavraArr)
 
-    if (primeiraPalavraArr.length > segundaPlavraArr.length) {
-        for (let i = 0; i < primeiraPalavraArr.length; i++) {
-            const res = segundaPlavraArr.indexOf(primeiraPalavraArr[i])
-            if (res === -1) {
-                return false
-            }        
-        }
+    if (res === -1 ) {
+        return false
     } else {
-        for (let i = 0; i < segundaPlavraArr.length; i++) {
-         
-        }
-        return res
-    }
-  }
-  
-  console.log(mutation(["hello", "hey"]));
+        return true
+    }        
+}
+
+  console.log(mutation(["hello","Hello"]));
