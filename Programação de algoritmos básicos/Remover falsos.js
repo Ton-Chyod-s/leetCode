f6a9a37d12d59b2b9040ba997a65414f921bec34
Klay <arqkdias@gmail.com>
2024-01-23 08:26:04 -0400

    Update Remover falsos.js

--- Programação de algoritmos básicos/Remover falsos.js
+++ Programação de algoritmos básicos/Remover falsos.js
@@ -1,11 +1,11 @@
 function bouncer(arr) {
-    const novoArr = [...arr];
-    for (let i = 0; i < novoArr.length; i++) {
-        if (novoArr[i] === false || novoArr[i] === null || novoArr[i] === 0 || novoArr[i] === "" || novoArr[i] === undefined || novoArr[i] === NaN) {
-            delete novoArr[i]
+    const novoArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]) {
+            novoArr.push(arr[i])
         } 
     }
     return novoArr;
   }
 
-console.log(bouncer([false, null, 0, NaN, undefined, ""]));
+console.log(bouncer([7, "ate", "", false, 9]));
