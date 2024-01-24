a5ed7a9d08a0aae861a0653972c72686e6e2858a
Klay <arqkdias@gmail.com>
2024-01-24 08:09:01 -0400

    Create Iterar sobre todas as propriedades.js

---
+++ Programação Orientada a Objetos/Iterar sobre todas as propriedades.js
@@ -0,0 +1,21 @@
+function Dog(name) {
+    this.name = name;
+  }
+  
+  Dog.prototype.numLegs = 4;
+  
+  let beagle = new Dog("Snoopy");
+  
+  let ownProps = [];
+  let prototypeProps = [];
+  
+  // Altere apenas o código abaixo desta linha
+  for (let i in beagle){
+    if (beagle.hasOwnProperty(i)) {
+        ownProps.push(i)
+    } else {
+        prototypeProps.push(i)
+    }
+  }
+  
+  
