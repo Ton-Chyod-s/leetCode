9fa1631f448539ab09b58503a0ca708ad16dd59d
Klay <arqkdias@gmail.com>
2024-01-31 08:53:39 -0400

    Update Redefinir uma propriedade herdada do construtor.js

--- Programação Orientada a Objetos/Redefinir uma propriedade herdada do construtor.js
+++ Programação Orientada a Objetos/Redefinir uma propriedade herdada do construtor.js
@@ -6,9 +6,6 @@ Bird.prototype = Object.create(Animal.prototype);
 Dog.prototype = Object.create(Animal.prototype);
 
 // Altere apenas o código abaixo desta linha
-
-
-
 let duck = new Bird();
 Bird.prototype = Bird;
 console.log(Bird.prototype)
