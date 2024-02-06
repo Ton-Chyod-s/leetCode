e4df824362b1db02fa3dcba9720d662c881a2135
Klay <arqkdias@gmail.com>
2024-02-06 16:59:09 -0400

    doc: att

--- LeetCode/20. Valid Parentheses.js
+++ LeetCode/20. Valid Parentheses.js
@@ -3,13 +3,25 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-   s = s.split("")
-   for (let i = 0; i < s.length; i++) {
-    if (s[i] === s[i+1]) {
-        continue;
-    } 
-    
+   const sList = new Array();
+   const map = {
+    '(': ')',
+    '[': ']',
+    '{': '}'
    }
+   for (let i = 0; i < s.length; i++) {
+        const caracter = s[i]
+        if (caracter in map) {
+            sList.push(caracter)
+        } else {
+            const retirarSLista = sList.pop();
+            if (caracter !== map[retirarSLista]) {
+                return false;
+            }
+        }
+   }
+ 
+   return !(s.length === 1 || (s.length === 2 && s[0] === s[1]));
 };
 
-console.log(isValid("()"))
+console.log(isValid("((("))
