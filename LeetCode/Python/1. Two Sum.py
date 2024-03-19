0caf9671a0ea45efa3f959fa805d2cc023a98589
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-03-19 16:43:42 -0400

    doc: att

--- LeetCode/Python/1. Two Sum.py
+++ LeetCode/Python/1. Two Sum.py
@@ -5,10 +5,13 @@ class Solution(object):
             end = len(nums)
             for keyJ, j in enumerate(nums[start:end], start):
                 if nums[keyI] + nums[keyJ] == target:
+                    if keyI == keyJ:
+                        continue
                     return [keyI,keyJ]
+                    
                 
 if __name__ == '__main__':
     nums = Solution()
-    #print(nums.twoSum([2,7,11,15],9))
+    print(nums.twoSum([2,7,11,15],9))
     print(nums.twoSum([3,2,4],6))
     print(nums.twoSum([2,5,5,11],10))
