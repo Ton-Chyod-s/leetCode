6195140b8938703c5a986ebd847d3ce25c7e5a5d
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2024-03-20 08:33:49 -0400

    doc: att

--- LeetCode/Python/2.Add-Two-Numbers.py
+++ LeetCode/Python/2.Add-Two-Numbers.py
@@ -5,11 +5,21 @@
 #         self.next = next
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: ListNode
-        :type l2: ListNode
-        :rtype: ListNode
-        """
+        l1.reverse()
+        l2.reverse()
+        listaNumL1 = ""
+        listaNumL2 = ""
+        for i in l1:
+            listaNumL1 += str(i)
+
+        for i in l2:
+            listaNumL2 += str(i)
+
+        res = int(listaNumL1) + int(listaNumL2)
+        res = [int(i) for i in str(res)]
+        res.reverse()
+
+        return res
 
 
 
@@ -18,3 +28,5 @@ if __name__ == '__main__':
     print(num.addTwoNumbers([2,4,3],[5,6,4]))
     print(num.addTwoNumbers([0],[0]))
     print(num.addTwoNumbers([9,9,9,9,9,9,9],[9,9,9,9]))
+
+    
